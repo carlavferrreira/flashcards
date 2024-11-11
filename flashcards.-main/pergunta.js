fdc2291bbe59e2e3284de70d7c997173bee9e81c
carlavferrreira <153856804+carlavferrreira@users.noreply.github.com>
2024-11-11 10:57:13 -0300

    Add files via upload

---
+++ flashcards.-main/pergunta.js
@@ -0,0 +1,23 @@
+criaCartao (
+    categoria,
+    pergunta,
+    resposta,
+)
+
+criaCartao(
+    "Programação",
+    "O que é JavaScript",
+    "JavaScript é uma linguagem de programação",
+)
+
+criaCartao(
+    "Geografia",
+    "Qual é a capital da França",
+    "A capotal da França é Paris",
+)
+
+criaCartao(
+    "Programação",
+    "O que é uma função",
+    "Uma função é um bloco de códio que executa uma tarefa",
+)
